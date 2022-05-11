ab208cb6c39f47dd4258438136088c9cd7e7abae
Saul Lopez <saul.lopez@oracle.com>
2022-05-11 13:09:33 -0500

    # Refactor code + testCreateJSON

--- vocs/REST.ino
+++ vocs/REST.ino
@@ -1,101 +1,116 @@
 #include "WiFi.h"
 #include "HTTPClient.h"
+#include "ArduinoJson.h"
 
-// Change ssid and password according at your local connection
-const char* ssid = "Wokwi-GUEST";
-const char* password = "";
+const char* SSID = "Wokwi-GUEST";
+const char* SSID_PASSWORD = "";
 const char* serverName = "https://g14fcd8d7fc5e27-db70cmj.adb.ca-toronto-1.oraclecloudapps.com/ords/iot/vocs/insertVoc/";
 
-String response = "";
+String serverResponse = "";
+String payloadPOST = "";
+
+
+const size_t CAPACITY = JSON_ARRAY_SIZE(3);
+
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  wifiConnect();
+  //wifiConnect(); // enable for send/read DataFromServer methods
 }
 
 void loop() {
-  sendData();
-  //testEndpointConnection();
+
+  //test_createJSON();
+  //test_sendDataToServer();
+  //test_readDataFromServer();
 
   delay(2000); //Wait two seconds for next joke
 }
 
 
-// This method executes an HTTP POST action
-void sendData() {
 
-  Serial.println("Sending Data");
+void testCreateJSON() {
+
+  StaticJsonDocument<CAPACITY> doc;
+
+  JsonObject obj = doc.createNestedObject();
+  obj["device_id"] = "dd-1";
+  obj["device_name"] = "Demo Device 1";
+  obj["temperature"] = "23";
+  obj["humidity"] = "33";
+  obj["toluene"] = "9876";
+
+  serializeJson(doc, payloadPOST);
+
+  Serial.println("JSON= " + payloadPOST);
+
+}
+
+
+
+void testSendDataToServer() {
 
   //Initiate HTTP client
   HTTPClient http;
 
-  //Start the request
+  //Start request
   http.begin(serverName);
 
+  //Define content-type to send
   http.addHeader("Content-Type", "application/json");
+  
+  //Execute POST request
+  int httpCode = http.POST("[{\"device_id\":88,\"device_name\":\"ESP32\",\"temperature\":\"90\",\"humidity\":\"90\",\"toluene\":\"111111\" }]");
+  
+  Serial.printf("[POST] response code: %d\n", httpCode);
 
-  int httpCode = http.POST("[{\"device_id\":90,\"device_name\":\"ESP32\",\"temperature\":\"90\",\"humidity\":\"90\",\"toluene\":\"111111\" }]");
-  Serial.printf("[HTTP] POST... code: %d\n", httpCode);
-
+  //Terminate connection
   http.end();
+
 }
 
 
-void testEndpointConnection() {
+void testReadDataFromServer() {
+
   //Initiate HTTP client
   HTTPClient http;
 
-  //Start the request
+  //Start request
   http.begin(serverName);
 
-  //Use HTTP GET request
+  //Execute GET request
   int httpCode = http.GET();
-  Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
-  //Response from server
-  response = http.getString();
+  Serial.printf("[GET] response code: %d\n", httpCode);
+  
+  // read response from server
+  serverResponse = http.getString();
 
-  Serial.print("RESPONSE: " + response);
+  Serial.print("RESPONSE: " + serverResponse);
+
+  //Terminate connection
   http.end();
-}
 
+}
 
 void wifiConnect() {
+
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, SSID_PASSWORD);
+
+  Serial.print("Stablishing connection with WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
+  Serial.println(" ");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.print("Mac address: ");
   Serial.println(WiFi.macAddress());
+
 }
-
-
-
-/*
-
- bulk 5 minutos
-JSON
-[
-    {
-      "device_id":1,
-      "device_name":"Uno",
-      "temperature":"30",
-      "humidity":"60",
-      "toluene":"1000"
-    },
-    {
-      "device_id":2,
-      "device_name":"Dos",
-      "temperature":"10",
-      "humidity":"20",
-      "toluene":"500"
-    }
-]
-*/
